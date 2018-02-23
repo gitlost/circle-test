5757ebae71f6071f4aeb95ef19a9a43ecf7a145c
gitlost <burmartke@gmail.com>
2018-02-23 02:39:47 +0000

    Initial version.

---
+++ circle-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Circle Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     circle-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Circle_Test
+ */
+
+// Your code starts here.
